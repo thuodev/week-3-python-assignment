d07a3c31d39eee64bacf4768e692cc2f4c9f5b8c
thuodev <samcreations104@gmail.com>
2025-08-16 15:16:43 +0300

    Add files via upload

---
+++ week 3 assignment.py
@@ -0,0 +1,14 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        return price - (price * discount_percent / 100)
+    else:
+        return price
+
+
+# --- Main program ---
+price = float(input("Enter the original price: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(price, discount_percent)
+
+print("Final price:", final_price)
